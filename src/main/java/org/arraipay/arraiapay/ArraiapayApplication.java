5822d51e9080e237a39fe3cb64b570d5e9a92b28
Teresa M. N. Egea <teresamnegea@proton.me>
2026-09-17 11:20:24 -0300

    Added project template to repository root

---
+++ src/main/java/org/arraipay/arraiapay/ArraiapayApplication.java
@@ -0,0 +1,13 @@
+package org.arraipay.arraiapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArraiapayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArraiapayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/arraipay/arraiapay/ArraiapayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.arraipay.arraiapay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArraiapayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
